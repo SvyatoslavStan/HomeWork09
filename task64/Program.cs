c49f68757a3ad83aa3c3cf80558e004257437a21
StanislavKhilyuk <geffie@yandex.ru>
2023-07-09 16:00:46 +0300

    task64, task 66, task68 and gitignore

---
+++ task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void number(int m, int n)
+{
+    if (m > n)
+    return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    
+    number(m+1,n);
+}
+
+number(m,n);
---
+++ task66/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum(int m, int n)
+{
+    if (m==n)
+    return n;
+    else return m + sum(m + 1, n);
+}
+
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {sum(m,n)}");
+
+
+
+// Console.WriteLine("Введите число M:");
+// int numberM = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите число N:");
+// int numberN = int.Parse(Console.ReadLine());
+
+// //Метод нахождения суммы натуральных элементов в промежутке от M до N
+// void NumberSum (int numberM, int numberN, int sum)
+// {
+//     if (numberM < numberN) 
+//     {
+//         sum = sum + (numberM++);
+//         return;
+//     }
+//     Console.Write($"Сумма натуральных элементов в промежутке от {numberM} до {numberN}: {sum}"); 
+//     NumberSum(numberM, numberN, sum);
+// }
+
+// NumberSum(numberM, numberN, 0);
